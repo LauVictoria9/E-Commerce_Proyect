7bc03761ce6c31bd535d18532ba3c1e16ba75b8a
Romero <aromeroarias7@gmail.com>
2023-09-19 00:33:35 -0500

    estructura de las paginas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import Index from "./layout";
+import Inicio from "./pages/Inicio";
+import Favoritos from "./pages/Favoritos";
+import Carrito from "./pages/Carrito";
+import User from "./pages/User";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Inicio />,
+      },
+      {
+        path: "favoritos/",
+        element: <Favoritos />,
+      },
+      {
+        path: "carrito/",
+        element: <Carrito />,
+      },
+      {
+        path: "user/",
+        element: <User />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/Inicio.jsx
@@ -0,0 +1,3 @@
+export default function Inicio() {
+  return <div className="min-h-[80vh] bg-gray-800 ">Inicio </div>;
+}
