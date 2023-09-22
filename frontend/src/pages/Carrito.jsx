3298914b3fe2f63f5b3dd67c87bc2a87b0cbb539
teban18 <jirafita163bob@gmail.com>
2023-09-22 14:56:07 -0500

    Estructura de encabezado

--- frontend/src/pages/Carrito.jsx
+++ frontend/src/pages/Carrito.jsx
@@ -1,3 +1,31 @@
 export default function Carrito() {
-  return <div>Carrito</div>;
+
+  {/*Página*/}
+  return <div className="m-0 p-0 box-border	font-sans">
+
+{/*Body*/}
+<div className="flex justify-center items-center	min-h-screen bg-[white]">
+
+{ /* Encabezado de articulos*/ }
+
+<div className="px-10 py-10 relative w-95 h-105 rounded-2 overflow-hidden border-b-2 rounded-lg border-slate-400">	
+
+    <h1 className="text-2xl text-violet-500	px-3 py-3">No tienes ningún artículo</h1>
+    <p className="text-center">Texto jajajjajajaaj</p>
+    
+    </div>
+  
+
+{
+  //Lista de articulos en el carrito
+}
+
+{
+  //Resumen de compra
+}
+
+
+
+  </div>
+</div>
 }
