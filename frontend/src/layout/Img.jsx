520fa74e8ddd2303e40640d20e251f47c16589f8
Laura Arana <aranalaura09@gmail.com>
2023-10-04 17:49:11 -0500

    Falta responsive

--- frontend/src/layout/Img.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Img() {
-  return (
-    <div className="card flex justify-content-center">
-      <Image src="../img/Elementos.png" alt="Image" width="250" />
-    </div>
-  );
-}
