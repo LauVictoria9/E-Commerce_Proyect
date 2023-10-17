07bc51413036f945f6a181ed3b151fee5adcfe9b
AlvaroJose1357 <joeibarrafernandez@gmail.com>
2023-10-17 18:14:35 -0500

    cambios en en models

--- backend/e_commerce_app/models.py
+++ backend/e_commerce_app/models.py
@@ -12,14 +12,14 @@ class tipo_producto(models.Model):
     def __str__(self):
         return self.nombre_tipoP +" "   + self.descripcion
 class producto(models.Model):
-   nombreProducto = models.CharField(max_length=100)
-   detalles = models.CharField(max_length=100)
-   precio = models.IntegerField(max_length=100)
-   cantidadProducto = models.IntegerField(max_length=100)
-   #imagen
-   tipo_producto = models.ForeignKey(tipo_producto, on_delete=models.CASCADE)
-   def __str__(self):
-        return self.tipo_producto.tipo_producto + " " + self.nombreProducto + " " + self.detalles + " " + self.precio + " " + self.cantidadProducto 
+    nombreProducto = models.CharField(max_length=100)
+    detalles = models.CharField(max_length=100)
+    precio = models.IntegerField(max_length=100)
+    cantidadProducto = models.IntegerField(max_length=100)
+    #imagen
+    tipo_producto = models.ForeignKey(tipo_producto, on_delete=models.CASCADE)
+    def __str__(self):
+            return self.tipo_producto.tipo_producto + " " + self.nombreProducto + " " + self.detalles + " " + self.precio + " " + self.cantidadProducto 
     
     
 
@@ -29,15 +29,15 @@ class rol(models.Model):
         return self.tipoDeUsuario
 
 #preguntar ahora usuario
-class user(models.Model):
-    tipoDeUsuario = models.ForeignKey(rol, on_delete=models.CASCADE)
-    nombreUser = models.CharField(max_length=100)
-    apellidoUser = models.CharField(max_length=100)
-    DireccionUser = models.CharField(max_length=100)
-    telefonoUser = models.CharField(max_length=100)
-    password= models.CharField(max_length=100)     
-    def __str__(self):
-        return self.rol.tipoDeUsuario # aqui se debe cambiar
+# class user(models.Model):
+#     tipoDeUsuario = models.ForeignKey(rol, on_delete=models.CASCADE)
+#     nombreUser = models.CharField(max_length=100)
+#     apellidoUser = models.CharField(max_length=100)
+#     DireccionUser = models.CharField(max_length=100)
+#     telefonoUser = models.CharField(max_length=100)
+#     password= models.CharField(max_length=100)     
+#     def __str__(self):
+#         return self.rol.tipoDeUsuario # aqui se debe cambiar
 
     
     #tambien para mirar metodoPago
@@ -49,7 +49,7 @@ class metodoPago(models.Model):
         return self.nombreMetodo+" "+self.descripcionMetodo
 
 class Venta_Producto(models.Model):
-    nombreUser = models.ForeignKey(user, on_delete=models.CASC)
+    nombreUser = models.ForeignKey(User, on_delete=models.CASC)
     nombreProducto = models.ForeignKey(producto, on_delete=models.CASCADE)
     cantidadTotal =models.IntegerField(max_length=100)
     fechaCompra = models.DateField()
