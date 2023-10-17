6cf564f60428fadfebd304c6cd4765a22345eba6
mr1113 <mauricioperez1399@gmail.com>
2023-10-17 17:57:48 -0500

    Navbar casi funcional

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import { BsFillPersonFill } from "react-icons/bs"; 
+import { AiOutlineShoppingCart } from "react-icons/ai"; 
+import { AiFillHeart } from "react-icons/ai";
 
 export default function Header() {
   return (
-    <header className="h-[10vh] bg-green-900">
-      <nav className="w-[85%] h-full m-auto flex justify-evenly items-center">
-        <Link to={"favoritos/"}>Favoritos</Link>
-        <Link to={"carrito/"}>Carrito</Link>
-        <Link to={"User/"}>Usuario</Link>
-      </nav>
+    <header className="h-[10vh]" >
+      <div className="flex flex-row-reverse my-2">
+      <BsFillPersonFill className="w-8 h-8"></BsFillPersonFill>
+      <AiOutlineShoppingCart className="w-8 h-8 mx-2"></AiOutlineShoppingCart>
+      <AiFillHeart className="w-8 h-8 "></AiFillHeart>
+      </div>
+     <Navbar/>
     </header>
   );
 }
