1f6e1004a251032f2c1317967cb3d621cd217c8c
Leydy Juliana <leydyjuliana06@gmail.com>
2023-10-19 20:45:13 -0500

    se creo la pagina de favoritos

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -1,13 +1,75 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+
+import { FaCartShopping, FaMagnifyingGlass } from "react-icons/fa6";
 
 export default function Header() {
+  const [showProfileOptions, setShowProfileOptions] = useState(false);
+
+  const handleClick = () => {
+    setShowProfileOptions(!showProfileOptions);
+  };
+
   return (
-    <header className="h-[10vh] bg-green-900">
-      <nav className="w-[85%] h-full m-auto flex justify-evenly items-center">
-        <Link to={"favoritos/"}>Favoritos</Link>
-        <Link to={"carrito/"}>Carrito</Link>
-        <Link to={"User/"}>Usuario</Link>
-      </nav>
+    <header className="px-8 py-4 bg-gradient-to-l to-violet-700 from-pink-700 text-white">
+      <div className="flex items-center">
+        <Link to="/">Logo</Link>
+
+        <div className="w-8" />
+
+        <nav className="font-medium">
+          <NavLink to={"/"} className="hover:underline">
+            Inicio
+          </NavLink>
+        </nav>
+
+        <div className="mx-auto relative items-center hidden md:flex">
+          <input
+            type="text"
+            placeholder="Buscar"
+            className="rounded-full px-2 py-1"
+          />
+
+          <FaMagnifyingGlass className="absolute right-2 text-black opacity-50" />
+        </div>
+
+        <div className="flex-grow md:hidden" />
+
+        <Link to={"/carrito"} className="text-2xl hover:opacity-90">
+          <FaCartShopping />
+        </Link>
+
+        <div className="w-4" />
+
+        <div className="flex justify-center relative">
+          <button
+            onClick={handleClick}
+            className="w-10 h-10 rounded-full bg-white"
+          ></button>
+
+          <div
+            className={`absolute ${
+              showProfileOptions ? "flex" : "hidden"
+            } z-10 text-center bg-gradient-to-br to-violet-700 from-pink-700 flex-col font-medium rounded-md -bottom-32 overflow-hidden`}
+          >
+            <Link
+              to={"/favoritos"}
+              className="px-4 py-2 hover:bg-black/25 transition-colors duration-200 ease-in-out"
+            >
+              Favoritos
+            </Link>
+            <Link
+              to={"/User"}
+              className="px-4 py-2 hover:bg-black/25 transition-colors duration-200 ease-in-out"
+            >
+              Perfil
+            </Link>
+            <button className="px-4 py-2 hover:bg-black/25 transition-colors duration-200 ease-in-out">
+              Salir
+            </button>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
