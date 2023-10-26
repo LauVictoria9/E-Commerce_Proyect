4a3a03246794c9b801f0406bd04ceefb6e5ce4a1
Deybi <deybi.perea@correounivalle.edu.co>
2023-10-25 20:12:45 -0500

    Seccion Lenovo y HP

--- frontend/src/pages/SeccionLenovo.jsx
+++ frontend/src/pages/SeccionLenovo.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import Asus from "../img/asusImage.png";
-import AsusPortatilImage from "../img/asusPortatil.jpg";
+import Lenovo from "../img/LenovoImage.png";
+import LenovoPortatilImage from "../img/AsusPortatil.jpg";
 
 /* Components imports */
 import Slider from "../components/Slider";
 
 /* Item data temp */
-import { itemDataAsus, itemDataAsus1, itemDataAsus2 } from "../layout/data";
+import { itemDataLenovo, itemDataLenovo1, itemDataLenovo2 } from "../layout/data";
 
-export default function SeccionAsus() {
+export default function SeccionLenovo() {
   return (
     <main className="pt-8 pb-16 ">
       <section className="relative flex justify-center items-center">
         <div className="bg-clr-two w-full h-96" />
-        <img src={Asus} alt="" className="absolute max-w-auto w-1/2" />
+        <img src={Lenovo} alt="" className="absolute max-w-auto w-1/2" />
       </section>
 
       <div className="h-8" />
@@ -22,7 +22,7 @@ export default function SeccionAsus() {
         <div className="flex">
           {/* Combo image */}
           <img
-            src={AsusPortatilImage}
+            src={LenovoPortatilImage}
             alt="Components combo image."
             className="w-1/3 hidden md:inline-block"
           />
@@ -31,7 +31,7 @@ export default function SeccionAsus() {
           <div className="w-full md:w-2/3 items-center relative flex">
             <div className="bg-gradient-to-br from-clr-two absolute -z-10 to-clr-three w-full h-4/5" />
 
-            <Slider sliderId="promo_one" data={itemDataAsus} />
+            <Slider sliderId="promo_one" data={itemDataLenovo} />
           </div>
         </div>
 
@@ -42,7 +42,7 @@ export default function SeccionAsus() {
           <div className="w-full md:w-4/% items-center relative flex">
             <div className="bg-gradient-to-br from-clr-two absolute -z-10 to-clr-three w-full h-4/5" />
 
-            <Slider sliderId="promo_two" data={itemDataAsus1} />
+            <Slider sliderId="promo_two" data={itemDataLenovo1} />
           </div>
         </div>
         <div className="h-16" />
@@ -52,7 +52,7 @@ export default function SeccionAsus() {
           <div className="w-full md:w-4/% items-center relative flex">
             <div className="bg-gradient-to-br from-clr-two absolute -z-10 to-clr-three w-full h-4/5" />
 
-            <Slider sliderId="promo_two" data={itemDataAsus2} />
+            <Slider sliderId="promo_three" data={itemDataLenovo2} />
           </div>
         </div>
       </section>
