9ca12e94d6ed38dc02fe4a0534a75ced27cb4143
romero2777 <aromeroarias7@gmail.com>
2023-11-20 22:48:45 -0500

    Update database, serializers and add filters API

--- backend/e_commerce_app/urls.py
+++ backend/e_commerce_app/urls.py
@@ -1,18 +1,18 @@
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework import routers
 from . import api
 from .views import LoginView, LogoutView
 
 router = routers.DefaultRouter()
 
-router.register('api/tipo-productos', api.TipoProductoViewSet, 'tipo-productos' )
-router.register('api/productos', api.ProductoViewSet, 'productos' )
-router.register('api/usuarios', api.UsuarioViewSet, 'usuarios' )
-router.register('api/metodo-pago', api.MetodoPagoViewSet, 'metodo-pago' )
-router.register('api/ventas', api.VentaViewSet, 'ventas' )
+router.register("api/tipo-productos", api.TipoProductoViewSet, "tipo-productos")
+router.register("api/productos", api.ProductoViewSet, "productos")
+router.register("api/usuarios", api.UsuarioViewSet, "usuarios")
+router.register("api/metodo-pago", api.MetodoPagoViewSet, "metodo-pago")
+router.register("api/ventas", api.VentaViewSet, "ventas")
 
 urlpatterns = [
-  path('auth/login/', LoginView.as_view(), name='auth_login'),
-  path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
+    path("auth/login/", LoginView.as_view(), name="auth_login"),
+    path("auth/logout/", LogoutView.as_view(), name="auth_logout"),
 ]
 urlpatterns += router.urls
